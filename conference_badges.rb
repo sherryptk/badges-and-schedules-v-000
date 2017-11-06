8f6035e651ecdd1aaac112b3cea1f81aba36c185
Sherry <sherryptk@gmail.com>
2017-11-06 14:50:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,12 @@
 # Write your code here.
 
 def badge_maker(name)
-  puts "Hello, my name is #{name}."  
+  puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(names)
+  messages = []
 
+  names.each do |name|
+    messages << badge_maker(name)
 end
