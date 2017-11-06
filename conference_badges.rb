4bf5027d1dfc8ad2122c718c78482d446bb7536b
Sherry <sherryptk@gmail.com>
2017-11-06 15:00:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,12 @@ end
 def assign_rooms(speakers)
   room_assignment = []
   num = 1
-  
+
   speakers.each do |name|
     room_assignment << "Hello, #{name}! You'll be assigned to room #{num}!"
     num=+
   end
+  
   room_assignment
+  
 end
